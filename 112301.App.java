78f7b8178af8cdedb971bb354fe09313ae7c99a6
HKM-interesting <74774357+HKM-interesting@users.noreply.github.com>
2020-11-23 15:13:08 +0800

    Add files via upload

---
+++ 112301.App.java
@@ -0,0 +1,24 @@
+package pac;
+
+import java.util.Scanner;
+
+public class App {
+	public static void main(String[] args) {
+	while(true){
+		System.out.println("ÊäÈë×ÖÄ¸A");
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		int x,y;
+		x=(int)(Math.random()*100);
+		y=(int)(Math.random()*100);
+		if(a.equals(a)){
+			System.err.println(x+","+y);
+		}else {
+			System.err.println("ÇëÖØÐÂÊäÈë£¡");
+	}
+	
+		}
+	}
+		
+		}
+
