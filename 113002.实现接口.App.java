d1cead36061006113ac9eba6ccc8794008ce5e64
HKM-interesting <74774357+HKM-interesting@users.noreply.github.com>
2020-11-30 15:34:00 +0800

    Create 113002.实现接口.App.java

---
+++ 113002.实现接口.App.java
@@ -0,0 +1,14 @@
+package pac;
+
+public class App implements R{
+	public static void main(String[] args) {
+		System.out.println(Bpp.boy_filePath);
+		System.out.println(Bpp.girl_filePath);
+		for (int i = 0; i < array2d.length; i++) {
+			for (int j = 0; j <array2d[i].length; j++) {
+				System.out.println(array2d[i][j]);
+			}
+		}
+		
+	}
+}
