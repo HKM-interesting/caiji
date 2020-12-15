aa81da780de24e89322dec3c3572812cdc9b3ed4
HKM-interesting <74774357+HKM-interesting@users.noreply.github.com>
2020-12-15 12:08:15 +0800

    Update 121403.闰年判断.App.java

--- 121403.闰年判断.App.java
+++ 121403.闰年判断.App.java
@@ -6,10 +6,20 @@ public class App {
         Scanner in = new Scanner(System.in);
         System.out.println("请输放年份");
         int year = in.nextInt();
-        if ((year % 4 == 0 && year % 100 != 00) || (year % 400 == 0)) {
-            System.out.println(year + "是一个闰年");
-        } else {
-            System.out.println(year + "是一个平年");
+        if (-3200 < year&&year < 3200) {
+            if (year<0){
+                if ((year % 4 == 1) || (year % 400 == 1)) {
+                    System.out.println(year + "是一个闰年");
+                } else {
+                    System.out.println(year + "是一个平年");
+                }
+            }else if (year>0){
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    System.out.println(year + "是一个闰年");
+                } else {
+                    System.out.println(year + "是一个平年");
+                }
+            }
         }
-    }
+        }
 }
